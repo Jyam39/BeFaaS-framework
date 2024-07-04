d79e4f36a304ff47f00ef1849af28169b6d1d064
JunyaYamamoto <yamamoto.junya@aic.cmc.osaka-u.ac.jp>
2024-07-04 15:04:31 +0900

    fix: Docker Hubのユーザ名を直接代入
    docker infoではDocker Hubのユーザ名を取得できない．

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,7 +22,8 @@ fi
 exp_dir="experiments/$1/functions"
 exp_config="experiments/$1/$exp_json"
 
-export DOCKERHUB_USER=`docker info 2>/dev/null | sed '/Username:/!d;s/.* //'`
+# DockerHubのユーザ名を取得する良い方法がないので，ひとまずベタ書き
+export DOCKERHUB_USER='jyam39'
 #Deactivate openFaaS (until node10-express template can be found)
 #command -v faas-cli >/dev/null && faas-cli template store pull node10-express #template pull ../node10-express-template #
 
